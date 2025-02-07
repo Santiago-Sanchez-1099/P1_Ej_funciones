3962e68acae739ff56f4c8c751bb40bd43d7dc5c
Santiago Sánchez Tinoco <164533981+Santiago-Sanchez-1099@users.noreply.github.com>
2025-02-07 16:47:45 +0000

    Mi primera vez

---
+++ funcion1.dart
@@ -0,0 +1,20 @@
+// funcion que no devuelve valor
+// funcion saludar
+void saludar(){
+  print('Hola mundo');
+}
+
+void escribeNombre(){
+  print('Santiago Sánchez');
+}
+
+void main(){
+  // Santiago Sánchez Mat: 22308051281099
+  // Llamar a la función saludar
+  print('Santiago Sánchez Mat: 22308051281099');
+  print('Llamando a la función saludar');
+  saludar();
+  print('Llamando a la función escribeNombre🚩🚩🚩🚩🚩🚩');
+  escribeNombre();
+
+}
