30b1b5206c5f9396796ea4614a1398e0af77dafa
Santiago Sánchez Tinoco <164533981+Santiago-Sanchez-1099@users.noreply.github.com>
2025-02-14 16:57:38 +0000

    mapa tabla 1

---
+++ mapatabla.dart
@@ -0,0 +1,50 @@
+void main() {
+  // MAPA PRODUCTO
+  Map<String, dynamic> producto = {
+    'id_producto': 201,
+    'nombre': 'Pan de trigo',
+    'descripcion': 'Pan integral hecho con harina de trigo',
+    'existencias': 50,
+    'precio': 2.99,
+    'fecha_ingreso': '2023-10-01',
+    'id_proveedor': 301,
+  };
+
+  // MAPA CLIENTE
+  Map<String, dynamic> cliente = {
+    'id_cliente': 101,
+    'nombre': 'Juan',
+    'apellidos': 'Pérez López',
+    'email': 'juan.perez@example.com',
+    'telefono': '555-1234',
+    'direccion': 'Calle Falsa 123, Ciudad Ejemplo',
+  };
+
+  // MAPA EMPLEADO
+  Map<String, dynamic> empleado = {
+    'id_empleado': 501,
+    'nombre': 'Ana',
+    'apellidos': 'Gómez Sánchez',
+    'email': 'ana.gomez@panaderia.com',
+    'puesto': 'Panadera',
+    'fecha_registro': '2022-05-15',
+  };
+
+  // DATOS DEL PRODUCTO
+  print('\nDatos del producto de panadería:');
+  producto.forEach((key, value) {
+    print('$key: $value');
+  });
+
+  // DATOS DEL CLIENTE
+  print('\nDatos del cliente:');
+  cliente.forEach((key, value) {
+    print('$key: $value');
+  });
+
+  // DATOS DEL EMPLEADO
+  print('\nDatos del empleado:');
+  empleado.forEach((key, value) {
+    print('$key: $value');
+  });
+}
---
+++ recursos/mapatabla.md
@@ -0,0 +1,8 @@
+Crear 3 mapas:
+<String, dinamic> productos (panaderia) con los siguientes key: id_producto, nombre, descripcion, existencias, precio, fecha_ingreso, id_proveedor.
+<String, dinamic> clientes con los siguientes key: id_cliente, nombre, apellidos, email, telefono, direccion.
+<String, dinamic> empleados con los siguientes key: id_empleado, nombre, apellidos, email, puesto, fecha_registro.
+Mostrar los datos con un for each. lenguaje dart
+
+Salida de datos:
+![alt text](image-9.png)
